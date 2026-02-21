a5cffd9288281dc4d4602f6a28f753ac78a64c86
dieuaime-shami <shamidieuaime272@gmail.com>
2026-02-21 17:26:50 +0200

    random memo generation

--- src/Components/HeadComponent.jsx
+++ src/Components/HeadComponent.jsx
@@ -1,8 +1,8 @@
 
 export default function HeadComponent(){
     return(
-        <div className="flex items-center gap-2 px-10 py-6 bg-violet-600">
-            <i class="fa-brands fa-optin-monster text-2xl text-amber-100"></i>
+        <div className="flex items-center gap-2 px-10 py-6 bg-purple-600">
+            <i className="fa-brands fa-optin-monster text-2xl text-amber-100"></i>
             <h1 className="text-white text-xl font-semibold">Meme Generator</h1>
         </div>
     )
--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,4 +1,37 @@
+
+import { useState,useEffect} from "react";
+
 export default function MemeComponent() {
+    const [meme, setMeme] = useState({
+        topText: "ONE DOES NOT SIMPLY",
+        bottomText: "WALK INTO MORDOR",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const[allMemes,setAllMemes]=useState([])
+
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data =>setAllMemes(data.data.memes))
+    },[])
+    
+    function getMemeImage(){
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        const newMemeUrl = allMemes[randomNumber].url;
+        setMeme( previous =>({
+            ...previous,
+            randomImage:newMemeUrl
+        }))
+    }
+    function handleEvents(event){
+        const {value,name}= event.target;
+        console.log(value) 
+        setMeme( previous =>({
+            ...previous,
+            [name]:value
+        }))
+    }
   return (
     <div className="max-w-2xl mx-auto bg-gray-100 p-6 rounded-lg">
 
@@ -11,6 +44,8 @@ export default function MemeComponent() {
             name="topText"
             placeholder="One does not simply"
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none  focus:ring-purple-500"
+            onChange={handleEvents}
+            value={meme.topText}
           />
         </div>
 
@@ -21,30 +56,30 @@ export default function MemeComponent() {
             name="bottomText"
             placeholder="Walk into Mordor"
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-purple-500"
+            onChange={handleEvents}
+            value={meme.bottomText}
           />
         </div>
       </div>
 
-      <button className="w-full  bg-purple-600 py-4 rounded-md font-semibold  mb-6">
+      <button className="w-full  bg-purple-600 py-4 rounded-md font-semibold  mb-6" onClick={getMemeImage}>
         Get a new meme image 🖼
       </button>
 
-      
       <div className="relative">
         <img
-          src="http://i.imgflip.com/1bij.jpg"
+          src={meme.randomImage}
           alt="generated meme"
           className="w-full rounded-md"
         />
 
-       
         <span className="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-3xl font-extrabold uppercase">
-          ONE DOES NOT SIMPLY
+         {meme.topText}
         </span>
 
        
         <span className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white text-3xl font-extrabold uppercase">
-          WALK INTO MORDOR
+          {meme.bottomText}
         </span>
       </div>
 
